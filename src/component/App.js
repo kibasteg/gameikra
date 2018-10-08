5a83b4d30008431b588fbb0aa9c587140fe69b22
pavelb <kibas@outlook.xom>
2018-10-08 22:50:36 +0300

    trololo

--- src/component/App.js
+++ src/component/App.js
@@ -6,18 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="ikra-app">
-          <div className="app">
-              <div className="app__header">
-                  HEADER
-              </div>
-              <div className="app__container">
-                  <Test />
-              </div>
-              <div className="app__footer">
-                  FOOTER
-              </div>
+          <div className="app__container">
+              <Test />
           </div>
-
       </div>
     );
   }
