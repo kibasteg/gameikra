9fd32bb672ca52b4914269e70fc1fc309524fa0a
pavelb <kibas@outlook.xom>
2018-10-10 08:52:41 +0300

    updated css

--- src/component/Answer.js
+++ src/component/Answer.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 class Answer extends React.Component {
 
+    handleClick = (answerId) => {
+
+        console.log(answerId, this);
+
+        this.props.handleClick(answerId);
+
+    };
+
     render () {
         return (
-            <div className="answer" onClick={() => {this.props.handleClick(this.props.answer.id)}}><span class="text">{this.props.answer.text}</span></div>
+            <div className="answer" onClick={() => {this.handleClick(this.props.answer.id)}}><span class="text">{this.props.answer.text}</span></div>
         );
     }
 
